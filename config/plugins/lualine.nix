0f64b45d59e2b8ed432643976e1ebef7f78e4385
Mykola Suprun <mykola.suprun@protonmail.com>
2024-02-28 11:35:53 +0000

    update flake

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -7,16 +7,16 @@
       "fugitive"
     ];
   };
-  extraConfigLua = ''
-    -- vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
-    local git_blame = require('gitblame')
-
-    require('lualine').setup({
-        sections = {
-                lualine_c = {
-                    { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
-                }
-        }
-    })
-  '';
+  # extraConfigLua = ''
+  #   -- vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+  #   local git_blame = require('gitblame')
+  #
+  #   require('lualine').setup({
+  #       sections = {
+  #               lualine_c = {
+  #                   { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+  #               }
+  #       }
+  #   })
+  # '';
 }
