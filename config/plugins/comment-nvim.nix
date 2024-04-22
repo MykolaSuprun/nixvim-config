46d27e8f21be85b068c3d33e367be452e0d8dbec
Mykola Suprun <mykola.suprun@protonmail.com>
2024-04-22 23:24:05 +0000

    treesitter-context,incremental select, and refactor

--- config/plugins/comment-nvim.nix
+++
@@ -1,6 +0,0 @@
-{
-  plugins.comment-nvim = {
-    enable = true;
-    padding = true;
-  };
-}
--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -1,8 +1,27 @@
 {
-  plugins.treesitter = {
-    enable = true;
-    # folding = true;
-    indent = true;
-    nixvimInjections = true;
+  plugins = {
+    treesitter = {
+      enable = true;
+      # folding = true;
+      indent = true;
+      nixvimInjections = true;
+      incrementalSelection = {
+        enable = true;
+        keymaps = {
+          initSelection = "<leader>ss";
+          nodeIncremental = "<leader>si";
+          nodeDecremental = "<leader>sd";
+          scopeIncremental = "<leader>sc";
+        };
+      };
+    };
+    treesitter-context.enable = true;
+    treesitter-refactor = {
+      enable = true;
+      highlightCurrentScope.enable = true;
+      highlightDefinitions.enable = true;
+      navigation.enable = true;
+      smartRename.enable = true;
+    };
   };
 }
