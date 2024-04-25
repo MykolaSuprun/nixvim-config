5cd365c5d77a60b00687f561883eaa917cbd3c42
Mykola Suprun <mykola.suprun@protonmail.com>
2024-04-25 01:51:12 +0000

    firenvim
    firenvim
    
    firenvim
    
    firenvim

--- config/plugins/firenvim.nix
+++ config/plugins/firenvim.nix
@@ -2,15 +2,11 @@
   extraPlugins = with pkgs.vimPlugins; [
     firenvim
   ];
-  extraConfigLua = ''
-    vim.api.nvim_create_autocmd({'UIEnter'}, {
-        callback = function(event)
-            local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
-            if client ~= nil and client.name == "Firenvim" then
-                -- code to execute on browser connect
-                vim.o.laststatus = 0
-            end
-        end
-    })
+  extraConfigLuaPost = ''
+    if vim.g.started_by_firenvim == true then
+      vim.o.laststatus = 0
+    else
+      vim.o.laststatus = 2
+    end
   '';
 }
