c8a3241f7fc3c0cb1ecd9d9a7021d7b1d6ebbd3d
Mykola Suprun <mykola.suprun@protonmail.com>
2024-05-29 18:32:43 +0200

    window movements

--- config/keymaps/window-movements.nix
+++ config/keymaps/window-movements.nix
@@ -39,5 +39,45 @@
       };
       action = "<cmd>TmuxNavigateUp<cr>";
     }
+    {
+      mode = "n";
+      key = "<leader>wv";
+      options = {
+        silent = true;
+        remap = true;
+        desc = "Vertical right split";
+      };
+      action = "<C-W>v";
+    }
+    {
+      mode = "n";
+      key = "<leader>ws";
+      options = {
+        silent = true;
+        remap = true;
+        desc = "Horizontal bottom split";
+      };
+      action = "<C-W>s";
+    }
+    {
+      mode = "n";
+      key = "<leader>wc";
+      options = {
+        silent = true;
+        remap = true;
+        desc = "Close window";
+      };
+      action = "<C-W>c";
+    }
+    {
+      mode = "n";
+      key = "<leader>wC";
+      options = {
+        silent = true;
+        remap = true;
+        desc = "Close other windows";
+      };
+      action = "<C-W>o";
+    }
   ];
 }
